35e44cb30b424aeeee5b58dce512db758919b116
gleitao <geovani.leitao@gmail.com>
2023-03-03 18:38:22 -0300

    receive hashid as parameter

--- scripts/glue/dimdatasources.py
+++ scripts/glue/dimdatasources.py
@@ -10,7 +10,7 @@ import delta
 import boto3
 import json
 
-args = getResolvedOptions(sys.argv, ["JOB_NAME", "datetime"])
+args = getResolvedOptions(sys.argv, ["JOB_NAME", "datetime", "hashid"])
 
 sc = SparkContext()
 glueContext = GlueContext(sc)
@@ -20,6 +20,37 @@ job.init("move_raw", args)
 job.commit()
 
 spark.conf.set("spark.sql.sources.partitionOverwriteMode", "dynamic")
+
+
+def insert_status(comments):
+    try:
+        df_control = spark.createDataFrame(
+            [(args["hashid"], datetime.now(), comments)],
+            ["hashid", "datetime", "comments"],
+        )
+
+        print(df_control.show())
+
+        client = boto3.client("secretsmanager", region_name="us-east-2")
+        get_secret_value_response = client.get_secret_value(SecretId="dev/trips-db")
+        secret = json.loads(get_secret_value_response["SecretString"])
+
+        (
+            df_control.write.format("jdbc")
+            .option("url", secret.get("host"))
+            .option("dbtable", "control.trips_load")
+            .option("user", secret.get("username"))
+            .option("password", secret.get("password"))
+            .mode("append")
+            .save()
+        )
+    except Exception as e:
+        raise ValueError(
+            f"Error when trying to salve data into data control process: {e}"
+        )
+
+
+insert_status("Running Dim Layer - DIMDATASOURCES")
 
 df = spark.read.format("parquet").load(
     f"s3://trips-datalake/raw/trips/datetime={args['datetime']}"
@@ -70,4 +101,5 @@ try:
         .save()
     )
 except Exception as e:
+    insert_status("Error when trying to salve data into redshift - DIMDATASOURCES")
     raise ValueError(f"Error when trying to salve data into redshift: {e}")
